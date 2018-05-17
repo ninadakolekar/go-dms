6ad1da87f598f548d52784972bbb42fd21be5d8e
Ninad Akolekar <ninad.akolekar@gmail.com>
2018-05-17 07:06:38 +0530

    Form Data Received on Server side through POST

--- src/controllers/processDocAdd.go
+++ src/controllers/processDocAdd.go
@@ -11,10 +11,25 @@ import (
 // ProcessDocAdd ... Process the form-values and add the document
 func ProcessDocAdd(w http.ResponseWriter, r *http.Request) {
 
-	docNumber := r.FormValue("docNumber")
+	if r.Method == "POST" {
 
-	fmt.Println("Form Received for docNumber ", docNumber) // Debug
+		r.ParseForm()
 
+		docNumber := r.Form["docNumber"][0]
+		docName := r.Form["docName"][0]
+		docProcess := r.Form["docProcess"][0]
+		docType := r.Form["docType"][0]
+		docDept := r.Form["docDept"][0]
+		docEffDate := r.Form["docEffDate"][0]
+		docExpDate := r.Form["docExpDate"][0]
+		docCreator := r.Form["docCreator"][0]
+		docAuth := r.Form["docAuth"]
+		docReviewers := r.Form["docReviewers"]
+		docApprovers := r.Form["docApprovers"]
+		fmt.Println("Form Received\n ", docNumber, docName, docProcess, docType, docDept, docEffDate, docExpDate, docCreator, docReviewers, docApprovers, docAuth) // Debug
+	}
+
+	// Render a new form
 	tmpl := template.Must(template.ParseFiles("templates/addNewDoc.html"))
 	tmpl.Execute(w, struct{ s uint }{constant.MinDocNumLen})
 }
