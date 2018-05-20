5a0e5be3266a333f255eebfd559a8a0a985ed205
Ninad Akolekar <ninad.akolekar@gmail.com>
2018-05-20 17:43:56 +0530

    Added checks in fetchDoc

--- src/docs/fetchDocByID.go
+++ src/docs/fetchDocByID.go
@@ -9,7 +9,7 @@ import (
 	solr "github.com/rtt/Go-Solr"
 )
 
-//FetchDocByID ... fetches
+//FetchDocByID ... Fetches the document with given ID from the database
 func FetchDocByID(uid string) (models.InactiveDoc, error) {
 
 	var doc models.InactiveDoc
@@ -36,38 +36,54 @@ func FetchDocByID(uid string) (models.InactiveDoc, error) {
 		err := errors.New("Document with id " + uid + "not found")
 		return doc, err
 	}
+
 	s1 := results.Get(0)
+
+	if !isDocValid(s1) {
+		err := errors.New("Invalid Document!\nMandatory fields found missing")
+		return doc, err
+	}
+
 	doc.DocNo = s1.Field("id").(string)
-	//fmt.Println("it came to line 59") //Debug
 	doc.Title = s1.Field("title").(string)
-	//fmt.Println("it came to line 61") //Debug
 	doc.DocType = s1.Field("docType").(string)
-	//fmt.Println("it came to line 63") //Debug
 	doc.DocProcess = s1.Field("docProcess").(string)
 	doc.DocEffDate = s1.Field("effDate").(string)
 	doc.DocExpDate = s1.Field("expDate").(string)
-	// doc.DocProcess = s1.Field("docProcess").(string)
 	doc.DocStatus = s1.Field("docStatus").(bool)
-	//fmt.Println("it came to line 65") //Debug
 	doc.Initiator = s1.Field("initiator").(string)
-	//fmt.Println("it came to line 67") //Debug
 	doc.Creator = s1.Field("creator").(string)
-	//fmt.Println("it came to line 69") //Debug
 	doc.DocDept = s1.Field("docDepartment").(string)
-	//fmt.Println("it came to line 71") //Debug
 	doc.FlowStatus = s1.Field("flowStatus").(float64)
-	//fmt.Println("it came to line 73") //Debug
 	doc.DocTemplate = s1.Field("docTemplateID").(float64)
-	//fmt.Println("it came to line 75") //Debug
-	// doc.DocTemplate = s1.Field("template").(string)
 	doc.InitTS = s1.Field("initTime").(string)
-	//fmt.Println("it came to line 77") //Debug
 
 	createTime := s1.Field("createTime")
 	if createTime == nil {
 		doc.CreateTS = ""
 	} else {
 		doc.CreateTS = s1.Field("createTime").(string)
+	}
+
+	reviewTime := s1.Field("reviewTime")
+	if reviewTime == nil {
+		doc.ReviewTS = ""
+	} else {
+		doc.ReviewTS = s1.Field("reviewTime").(string)
+	}
+
+	approveTime := s1.Field("approveTime")
+	if approveTime == nil {
+		doc.ApproveTS = ""
+	} else {
+		doc.ApproveTS = s1.Field("approveTime").(string)
+	}
+
+	authTime := s1.Field("authTime")
+	if authTime == nil {
+		doc.AuthTS = ""
+	} else {
+		doc.AuthTS = s1.Field("authTime").(string)
 	}
 
 	temp, ok := s1.Field("reviewer").([]interface{})
@@ -81,6 +97,7 @@ func FetchDocByID(uid string) (models.InactiveDoc, error) {
 			}
 		}
 	}
+
 	temp, ok = s1.Field("approver").([]interface{})
 	if ok {
 		for _, v := range temp {
@@ -92,16 +109,29 @@ func FetchDocByID(uid string) (models.InactiveDoc, error) {
 			}
 		}
 	}
-	temp, ok = s1.Field("authorizer").([]interface{})
-	if ok {
-		for _, v := range temp {
-			item, okk := v.(string)
-			if okk {
-				doc.Authorizer = append(doc.Authorizer, item)
-			} else {
-				break
+
+	if s1.Field("authorizer") != nil {
+		temp, ok := s1.Field("authorizer").([]interface{})
+		if ok {
+			for _, v := range temp {
+				item, okk := v.(string)
+				if okk {
+					doc.Authorizer = append(doc.Authorizer, item)
+				} else {
+					break
+				}
 			}
 		}
 	}
+
 	return doc, nil
+}
+
+func isDocValid(s1 *solr.Document) bool {
+
+	if s1.Field("id") != nil && s1.Field("title") != nil && s1.Field("docType") != nil && s1.Field("docProcess") != nil && s1.Field("effDate") != nil && s1.Field("expDate") != nil && s1.Field("docStatus") != nil && s1.Field("initiator") != nil && s1.Field("creator") != nil && s1.Field("docDepartment") != nil && s1.Field("flowStatus") != nil && s1.Field("docTemplateID") != nil && s1.Field("initTime") != nil && s1.Field("approver") != nil && s1.Field("reviewer") != nil {
+		return true
+	}
+
+	return false
 }
