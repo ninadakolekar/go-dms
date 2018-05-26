e4f9498e7e54e4d9aa436a3d57fdeccb44c97298
Ninad Akolekar <ninad.akolekar@gmail.com>
2018-05-26 18:35:40 +0530

    Dashboard UI and auth

--- src/controllers/dashboard.go
+++ src/controllers/dashboard.go
@@ -1,12 +1,27 @@
 package controllers
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/ninadakolekar/aizant-dms/src/user"
 )
 
 //Login ... login
 func Dashboard(w http.ResponseWriter, r *http.Request) {
+
+	user, err := user.GetCurrentUser(r)
+
+	fmt.Println(user) //Debug
+
+	if err != nil { // Login unsucessful
+		http.Redirect(w, r, "/", 302)
+		return
+	}
+
 	tmpl := template.Must(template.ParseFiles("templates/dashboard.html"))
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, struct {
+		Username string
+	}{user})
 }
