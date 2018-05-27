3933ea8d4535996800928359b47688815c8d5450
Ninad Akolekar <ninad.akolekar@gmail.com>
2018-05-27 11:04:04 +0530

    Incremented flow status after init; removed unnecessary debug stmts

--- src/controllers/processDocAddEdit.go
+++ src/controllers/processDocAddEdit.go
@@ -92,7 +92,7 @@ func ProcessDocAddEdit(w http.ResponseWriter, r *http.Request) {
 				Approver:     docApprovers,
 				Authorizer:   docAuth,
 				DocDept:      docDept,
-				FlowStatus:   0,
+				FlowStatus:   1,
 				DocTemplate:  0,
 				InitTS:       initTime,
 				CreateTS:     "",
